8abf12a905e992e7713d4541d13b5bb952e9bb9e
MOHAMEDRIASKHAN <mohamedriaskhan@gmail.com>
2022-09-09 08:36:09 -0700

    Welcome Message

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Text.RegularExpressions;
+
+namespace LamdaPracticeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Lambda Practice Problem");
+
+
+
+
+            
+    
