3ed3ba48cf11c21d14b7bcff20a90a9cfa226542
MOHAMEDRIASKHAN <108645671+MOHAMEDRIASKHAN@users.noreply.github.com>
2022-09-09 08:09:08 -0700

    UC_13 using Lambda Expression

---
+++ PersonDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LamdaPracticeProblem
+{
+    public class PersonDetails
+    {
+        public string FirstName;
+        public string LastName;
+        public string Email;
+        public string PhoneNumber;
+        public string Password;
+
+        public PersonDetails(string firstName, string lastName, string email, string phoneNumber, string password)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -12,6 +12,52 @@ namespace LamdaPracticeProblem
 
             Console.WriteLine("Welcome to Lambda Practice Problem");
 
+             string Regex_FirstName = "^[A-Z]{5}$";  //RIYAS
+
+             string Regex_LastName = "^[A-Z]{4}";   //KHAN
+
+             string Regex_MobileNumber = "^([0-9]{2}[ ])?[0-9]{10}$";  //91 9876543210
+
+             string Regex_Password = @"^([A-Z]{1}[a-z]{4}[@][0-9]{4]$";  //Riyas@1234
+
+             string Regex_Email = "^[a-z]{9}[@][a-z]{5}[.][a-z]{3}?$";   //riyaskhan@gmail.com
+
+            
+            
+                Console.WriteLine("=====================================================User Registration===============================================");
+                Console.Write("\nPlease enter user details\n");
+
+                Console.Write("First Name:     ");
+                string firstName = Console.ReadLine();
+
+                Console.Write("Last Name:     ");
+                string lastName = Console.ReadLine();
+
+                Console.Write("Email:     ");
+                string email = Console.ReadLine();
+
+                Console.Write("Mobile Number:     ");
+                string mobile = Console.ReadLine();
+
+                Console.Write("Password:     ");
+                string password = Console.ReadLine();
+
+                Func<string, bool> isValidName = s => Regex.IsMatch(s, Regex_FirstName);
+                Func<string, bool> isValidMobileNumber = s => Regex.IsMatch(s, Regex_MobileNumber);
+                Func<string, bool> isValidPassword = s => Regex.IsMatch(s, Regex_Password);
+                Func<string, bool> isValidEmail = s => Regex.IsMatch(s, Regex_Email);
+
+            Console.WriteLine("\n{0}\n{1}\n{2}\n{3}\n{4} ",
+                              (isValidName(firstName)) ? "First name is valid" : "First name is invalid",
+                              (isValidName(lastName)) ? "Last name is valid" : "Last name is invalid",
+                              (isValidEmail(email)) ? "Email address is valid" : "Email address is invalid",
+                              (isValidMobileNumber(mobile)) ? "Mobile number is valid" : "Mobile number is invalid",
+                              (isValidPassword(password)) ? "Password is valid" : "Password is invalid");
+                
+
+            }
+        }
+    }
 
 
 
